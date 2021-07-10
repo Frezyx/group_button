d7da76b137933c8c96174b1761f3e5c5322639f3
Stanislav Ilin <youngfrezyx@gmail.com>
2021-07-10 15:26:59 +0500

    Refactor example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -33,122 +33,128 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("group_button 2.4.1"),
-          bottom: TabBar(
-            tabs: [
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("group_button 2.4.1"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
               Tab(icon: _buildRadioTabIcon()),
               Tab(icon: _buildCheckTabIcon()),
               Tab(icon: _buildCustomTabIcon()),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Center(
-                child: GroupButton(
-                  spacing: 10,
-                  direction: direction,
-                  onSelected: (index, isSelected) =>
-                      print('$index button is selected'),
-                  buttons: [
-                    "12:00",
-                    "13:00",
-                    "14:30",
-                    "18:00",
-                    "19:00",
-                    "21:40",
-                  ],
-                  selectedButton: 5,
+          Expanded(
+            child: PageView(
+              children: [
+                _buildRadioExample(GroupingType.column),
+                _buildRadioExample(GroupingType.wrapp),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                    child: GroupButton(
+                      spacing: 15,
+                      isRadio: false,
+                      direction: direction,
+                      onSelected: (index, isSelected) => print(
+                          '$index button is ${isSelected ? 'selected' : 'unselected'}'),
+                      buttons: [
+                        "Burger",
+                        "Sandwiches",
+                        "Salad",
+                        "Carbonara",
+                        "Meat",
+                        "French fries",
+                        "Carbonated beverage"
+                      ],
+                      selectedButtons: [2, 3],
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GroupButton(
+                    spacing: 5,
+                    isRadio: false,
+                    direction: direction,
+                    onSelected: (index, isSelected) => print(
+                        '$index button is ${isSelected ? 'selected' : 'unselected'}'),
+                    buttons: [
+                      "Dart",
+                      "Kotlin",
+                      "Java",
+                      "Swift",
+                      "Objective-C",
+                      "Python",
+                      "JS",
+                      "C#",
+                      "C",
+                      "C++"
+                    ],
+                    selectedTextStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.red,
+                    ),
+                    unselectedTextStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: Colors.grey[600],
+                    ),
+                    selectedColor: Colors.white,
+                    unselectedColor: Colors.grey[300],
+                    selectedBorderColor: Colors.red,
+                    unselectedBorderColor: Colors.grey[500],
+                    borderRadius: BorderRadius.circular(5.0),
+                    selectedShadow: <BoxShadow>[
+                      BoxShadow(color: Colors.transparent)
+                    ],
+                    unselectedShadow: <BoxShadow>[
+                      BoxShadow(color: Colors.transparent)
+                    ],
+                    buttonHeight: 30,
+                    buttonWidth: 115,
+                    mainGroupAlignment: MainGroupAlignment.center,
+                    crossGroupAlignment: CrossGroupAlignment.center,
+                    groupRunAlignment: GroupRunAlignment.spaceEvenly,
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Center(
-                child: GroupButton(
-                  spacing: 15,
-                  isRadio: false,
-                  direction: direction,
-                  onSelected: (index, isSelected) => print(
-                      '$index button is ${isSelected ? 'selected' : 'unselected'}'),
-                  buttons: [
-                    "Burger",
-                    "Sandwiches",
-                    "Salad",
-                    "Carbonara",
-                    "Meat",
-                    "French fries",
-                    "Carbonated beverage"
-                  ],
-                  selectedButtons: [
-                    2,
-                    3,
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: GroupButton(
-                spacing: 5,
-                isRadio: false,
-                direction: direction,
-                onSelected: (index, isSelected) => print(
-                    '$index button is ${isSelected ? 'selected' : 'unselected'}'),
-                buttons: [
-                  "Dart",
-                  "Kotlin",
-                  "Java",
-                  "Swift",
-                  "Objective-C",
-                  "Python",
-                  "JS",
-                  "C#",
-                  "C",
-                  "C++"
-                ],
-                selectedTextStyle: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                  color: Colors.red,
-                ),
-                unselectedTextStyle: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 14,
-                  color: Colors.grey[600],
-                ),
-                selectedColor: Colors.white,
-                unselectedColor: Colors.grey[300],
-                selectedBorderColor: Colors.red,
-                unselectedBorderColor: Colors.grey[500],
-                borderRadius: BorderRadius.circular(5.0),
-                selectedShadow: <BoxShadow>[
-                  BoxShadow(color: Colors.transparent)
-                ],
-                unselectedShadow: <BoxShadow>[
-                  BoxShadow(color: Colors.transparent)
-                ],
-                buttonHeight: 30,
-                buttonWidth: 115,
-                mainGroupAlignment: MainGroupAlignment.center,
-                crossGroupAlignment: CrossGroupAlignment.center,
-                groupRunAlignment: GroupRunAlignment.spaceEvenly,
-              ),
-            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          direction =
+              direction == Axis.horizontal ? Axis.vertical : Axis.horizontal;
+        }),
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+
+  Padding _buildRadioExample(GroupingType groupingType) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Center(
+        child: GroupButton(
+          spacing: 10,
+          direction: direction,
+          groupingType: groupingType,
+          onSelected: (index, isSelected) => print('$index button is selected'),
+          buttons: [
+            "12:00",
+            "13:00",
+            "14:30",
+            "18:00",
+            "19:00",
+            "21:40",
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() {
-            direction =
-                direction == Axis.horizontal ? Axis.vertical : Axis.horizontal;
-          }),
-          child: Icon(Icons.refresh),
+          selectedButton: 5,
         ),
       ),
     );
