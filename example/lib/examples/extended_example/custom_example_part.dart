88f995a3a9a3080d2c5a1b05ef9ddc4630510e35
Stanislav Ilin <youngfrezyx@gmail.com>
2021-07-11 10:13:50 +0500

    Implement scroll ijector

--- example/lib/examples/extended_example/custom_example_part.dart
+++ example/lib/examples/extended_example/custom_example_part.dart
@@ -1,49 +1,52 @@
 part of 'example.dart';
 
-Padding _buildCustomExample(GroupingType groupingType) {
-  return Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: GroupButton(
-      spacing: 5,
-      isRadio: false,
-      groupingType: groupingType,
-      // direction: direction,
-      onSelected: (index, isSelected) =>
-          print('$index button is ${isSelected ? 'selected' : 'unselected'}'),
-      buttons: [
-        "Dart",
-        "Kotlin",
-        "Java",
-        "Swift",
-        "Objective-C",
-        "Python",
-        "JS",
-        "C#",
-        "C",
-        "C++"
-      ],
-      selectedTextStyle: TextStyle(
-        fontWeight: FontWeight.w600,
-        fontSize: 16,
-        color: Colors.red,
+Widget _buildCustomExample(GroupingType groupingType) {
+  return ScrollIjector(
+    groupingType: groupingType,
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: GroupButton(
+        spacing: 5,
+        isRadio: false,
+        groupingType: groupingType,
+        // direction: direction,
+        onSelected: (index, isSelected) =>
+            print('$index button is ${isSelected ? 'selected' : 'unselected'}'),
+        buttons: [
+          "Dart",
+          "Kotlin",
+          "Java",
+          "Swift",
+          "Objective-C",
+          "Python",
+          "JS",
+          "C#",
+          "C",
+          "C++"
+        ],
+        selectedTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 16,
+          color: Colors.red,
+        ),
+        unselectedTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 14,
+          color: Colors.grey[600],
+        ),
+        selectedColor: Colors.white,
+        unselectedColor: Colors.grey[300],
+        selectedBorderColor: Colors.red,
+        unselectedBorderColor: Colors.grey[500],
+        borderRadius: BorderRadius.circular(5.0),
+        selectedShadow: <BoxShadow>[BoxShadow(color: Colors.transparent)],
+        unselectedShadow: <BoxShadow>[BoxShadow(color: Colors.transparent)],
+        buttonHeight: 30,
+        buttonWidth: 115,
+        mainGroupAlignment: MainGroupAlignment.center,
+        crossGroupAlignment: CrossGroupAlignment.center,
+        groupRunAlignment: GroupRunAlignment.spaceEvenly,
       ),
-      unselectedTextStyle: TextStyle(
-        fontWeight: FontWeight.w600,
-        fontSize: 14,
-        color: Colors.grey[600],
-      ),
-      selectedColor: Colors.white,
-      unselectedColor: Colors.grey[300],
-      selectedBorderColor: Colors.red,
-      unselectedBorderColor: Colors.grey[500],
-      borderRadius: BorderRadius.circular(5.0),
-      selectedShadow: <BoxShadow>[BoxShadow(color: Colors.transparent)],
-      unselectedShadow: <BoxShadow>[BoxShadow(color: Colors.transparent)],
-      buttonHeight: 30,
-      buttonWidth: 115,
-      mainGroupAlignment: MainGroupAlignment.center,
-      crossGroupAlignment: CrossGroupAlignment.center,
-      groupRunAlignment: GroupRunAlignment.spaceEvenly,
     ),
   );
 }
--- example/lib/examples/extended_example/radio_example_part.dart
+++ example/lib/examples/extended_example/radio_example_part.dart
@@ -1,23 +1,26 @@
 part of 'example.dart';
 
-Padding _buildRadioExample(GroupingType groupingType) {
-  return Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: Center(
-      child: GroupButton(
-        spacing: 10,
-        // direction: direction,
-        groupingType: groupingType,
-        onSelected: (index, isSelected) => print('$index button is selected'),
-        buttons: [
-          "12:00",
-          "13:00",
-          "14:30",
-          "18:00",
-          "19:00",
-          "21:40",
-        ],
-        selectedButton: 5,
+Widget _buildRadioExample(GroupingType groupingType) {
+  return ScrollIjector(
+    groupingType: groupingType,
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Center(
+        child: GroupButton(
+          spacing: 10,
+          // direction: direction,
+          groupingType: groupingType,
+          onSelected: (index, isSelected) => print('$index button is selected'),
+          buttons: [
+            "12:00",
+            "13:00",
+            "14:30",
+            "18:00",
+            "19:00",
+            "21:40",
+          ],
+          selectedButton: 5,
+        ),
       ),
     ),
   );
