b96657f5f88143b4b275ed423727175eec8b23ab
Stanislav Ilin <youngfrezyx@gmail.com>
2021-07-12 20:59:35 +0500

    Add common example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,9 +7,32 @@ import 'package:group_button/group_button.dart';
 /// You can use [GroupButtonExtendedExample] example
 /// for check all package [GroupingType]'s in one place
 
+/// /// You can use [StylesExample] example
+/// for check all package style's configuration
+
 /// You can use [GroupButtonProviderExample] example
 /// using this package with state - managment package like provider
 
 void main() {
-  runApp(StylesExample());
+  runApp(CommonExample());
+}
+
+class CommonExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: GroupButton(
+            spacing: 10,
+            buttons: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
+            groupingType: GroupingType.wrap,
+            borderRadius: BorderRadius.circular(30),
+            onSelected: (i, selected) => print('Button #$i selected'),
+          ),
+        ),
+      ),
+    );
+  }
 }
