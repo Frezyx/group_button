da962acb863652c4fa21649bf79849e8dd886687
Stanislav Ilin <youngfrezyx@gmail.com>
2021-12-28 22:07:35 +0500

    Add simple docs

--- lib/src/utils/controller.dart
+++ lib/src/utils/controller.dart
@@ -4,19 +4,25 @@ class GroupButtonController extends ChangeNotifier {
   int _selectedIndex = 0;
   final _selectedIndexes = <int>{};
 
+  /// Selected button index in case when you using radio type
   int get selectedIndex => _selectedIndex;
+
+  /// Selected buttons indexes in case when you using checkbox type
   Set<int> get selectedIndexes => _selectedIndexes;
 
+  /// Select button by index in radio type
   void selectIndex(int i) {
     _selectedIndex = i;
     notifyListeners();
   }
 
+  /// Unselect all buttons in checkbox type
   void unselectAll() {
     _selectedIndexes.clear();
     notifyListeners();
   }
 
+  /// Toggle buttons by indexes in checkbox type
   void toggleIndexes(List<int> indexes) {
     for (final i in indexes) {
       if (_selectedIndexes.contains(i)) {
@@ -28,11 +34,13 @@ class GroupButtonController extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Select buttons by indexes in checkbox type
   void selectIndexes(List<int> indexes) {
     _selectedIndexes.addAll(indexes);
     notifyListeners();
   }
 
+  /// Unselect buttons by indexes in checkbox type
   void unselectIndexes(List<int> indexes) {
     _selectedIndexes.removeAll(indexes);
     notifyListeners();
