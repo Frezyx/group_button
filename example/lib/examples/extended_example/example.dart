b7af5819847466cb4c25bd5a6fcc9f6845bcab7b
Stanislav Ilin <youngfrezyx@gmail.com>
2021-12-29 01:52:21 +0500

    Update version to 4.0.0+1

--- example/lib/examples/extended_example/example.dart
+++ example/lib/examples/extended_example/example.dart
@@ -45,7 +45,7 @@ class _ExampleState extends State<_Example> {
         shadowColor: Colors.grey[100].withOpacity(0.1),
         backgroundColor: Colors.white,
         title: Text(
-          'GroupButton 4.0.0',
+          'GroupButton 4.0.0+1',
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
